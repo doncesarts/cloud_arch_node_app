46e0ad4821e80127dca034f91a1ee9ff43124ef1
Daniel Rejniak <drejniak@ie.ibm.com>
2017-11-29 19:27:12 +0000

    Fix deplyment shell

--- deploy_to_docker_repo.sh
+++ deploy_to_docker_repo.sh
@@ -1,8 +1,8 @@
 
 #!/bin/bash
 
-image_name="ca674_cloud_arch_test_app"
+image_name="ca674_cloud_arch_app"
 
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS $DOCKER_HOST &&
-docker build -t $image_name . &&
+docker build -t danielrejniak/$image_name . &&
 docker push danielrejniak/$image_name
