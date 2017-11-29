faada364525cdae6600d126654ee6f6340f97cfa
Daniel Rejniak <drejniak@ie.ibm.com>
2017-11-29 19:51:13 +0000

    Added os dependancy and andded hostname echo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+var os = require("os");
 var visitCounter = 0;
 
 // Constants
@@ -9,9 +10,17 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
+
+//Couter route
+app.get('/counter', (req, res) => {
   visitCounter++;
   res.send('Application Hase Been Visited ' + visitCounter + ' Times');
+});
+
+//Hostname echo router
+app.get('/hostname', (req, res) => {
+  visitCounter++;
+  res.send('Hello Fron ' + os.hostname());
 });
 
 app.listen(PORT, HOST);
