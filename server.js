39f31b1320ec1cefd56c42afc0dd9a2124d308ee
doncesarts <doncesarts@gmail.com>
2017-12-02 19:42:15 +0000

    Cloud Foundry App Migration

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
+const cfenv = require("cfenv");
 var os = require("os");
 var visitCounter = 0;
 
+// get environmental information for this App
+const appEnv = cfenv.getAppEnv();
+const instance = appEnv.app.instance_index || 0;
+
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = appEnv.port;
+const HOST = appEnv.bind;
 
 // App
 const app = express();
 
 //Couter route
-app.get('/counter', (req, res) => {
+app.get("/counter", (req, res) => {
   visitCounter++;
-  res.send('Application Hase Been Visited ' + visitCounter + ' Times');
+  res.send("Application Hase Been Visited " + visitCounter + " Times");
 });
 
 //Hostname echo router
-app.get('/hostname', (req, res) => {
+app.get("/hostname", (req, res) => {
   visitCounter++;
-  res.send('Hello Fron ' + os.hostname());
+  res.send("Hello From " + os.hostname());
 });
 
 app.listen(PORT, HOST);
